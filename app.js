78249056f58ad1ed5bab0fd59aad7e498a2df119
munish710 <munish710@gmail.com>
2021-08-22 02:02:56 +0530

    added route not found handler

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const tasksRoutes = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const notFound = require("./middlewares/not-found");
+
 require("dotenv").config();
 
 const app = express();
 
 //middlewares
 app.use(express.json());
+app.use(express.static("./public"));
 
 //routes
-app.get("/", (req, res) => {
-  res.send("Hola Amigos");
-});
-
 app.use("/api/v1/tasks", tasksRoutes);
+
+app.use(notFound);
 
 const port = 5000;
 
