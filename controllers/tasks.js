e3934e0ee19493c33c8047bdff0f28b09c60a5b0
munish710 <munish710@gmail.com>
2021-08-22 00:42:45 +0530

    completed all the controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,12 @@
 const Task = require("../models/task");
 
-const getAllTasks = (req, res) => {
-  res.send("get all Tasks");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(201).json({ tasks });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 const createTask = async (req, res) => {
@@ -13,17 +18,34 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  //   res.send("get single task");
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  const { id: taskID } = req.params;
+  try {
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with ${taskID} found` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 const updateTask = (req, res) => {
   res.send("update task");
 };
 
-const deleteTask = (req, res) => {
-  res.send("delete Task");
+const deleteTask = async (req, res) => {
+  const { id: taskID } = req.params;
+  try {
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with ${taskID} found` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 module.exports = {
