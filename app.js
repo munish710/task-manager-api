c8a91b7af9d189f6187846a56a14324865bb6b7e
munish710 <munish710@gmail.com>
2021-08-21 14:40:12 +0530

    basic express server

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Task Manager App");
+const express = require("express");
+
+const app = express();
+
+//routes
+app.get("/", (req, res) => {
+  res.send("Hola Amigos");
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
