b9e559910d114e9b43589fab63ebd216f3bc739f
munish710 <munish710@gmail.com>
2021-08-21 15:12:38 +0530

    setup basic routes for the api

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const tasksRoutes = require("./routes/tasks");
 
 const app = express();
+
+//middlewares
+app.use(express.json());
 
 //routes
 app.get("/", (req, res) => {
   res.send("Hola Amigos");
 });
+
+app.use("/api/v1/", tasksRoutes);
 
 const port = 5000;
 
